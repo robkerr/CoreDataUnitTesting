7ad7fafed2522acc754925c5111cbfc912cc6d0b
Rob Kerr <rob@kerr.cc>
2018-06-16 07:37:58 -0400

    Initial Commit

---
+++ CoreDataUnitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataUnitTesting
+//
+//  Created by Robert Kerr on 6/16/18.
+//  Copyright © 2018 kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
